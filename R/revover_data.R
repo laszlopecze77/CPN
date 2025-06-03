cdb4de3b37751d10ca6fe9698a3ee9df94d9731f
F. Collin, PhD <fca.collin@gmail.com>
2025-06-03 09:51:20 +0200

    automatic reflow (ctrl+shift+/)

--- R/revover_data.R
+++ R/revover_data.R
@@ -1,16 +1,18 @@
 #' Recover Data for `cpn` Model Objects
 #'
-#' This method is used by the \pkg{emmeans} package to recover the data and terms
-#' from a fitted Compound Poisson-Normal (CPN) model object. It is typically used
-#' internally when computing estimated marginal means.
+#' This method is used by the \pkg{emmeans} package to recover the data and
+#' terms from a fitted Compound Poisson-Normal (CPN) model object. It is
+#' typically used internally when computing estimated marginal means.
 #'
-#' @param object A fitted model object of class \code{cpn}.
+#' @param object A fitted model object of class `cpn`.
 #' @param ... Additional arguments (not used).
 #'
-#' @return An object containing the recovered data and terms, suitable for use by \pkg{emmeans}.
+#' @return An object containing the recovered data and terms, suitable for use
+#'   by \pkg{emmeans}.
 #'
 #' @importFrom emmeans recover_data
 #' @export
+#'
 recover_data.cpn <- function(object, ...) {
   emmeans:::recover_data.call(
     object$call,
